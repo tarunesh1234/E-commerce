362e8a8a5b5f3d9dccbb758496600ddd0c00f543
“tarunesh1234” <taruneshkumar1234@gmail.com>
2018-01-03 00:37:22 +0530

    crispy forms added

--- tryTen/settings.py
+++ tryTen/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'profiles',
     'contact',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
